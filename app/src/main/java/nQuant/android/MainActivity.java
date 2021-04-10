0d2a2265a62aca809fb34488609127986f9ae861
Miller Cy Chan <miller.chan@gmail.com>
2021-04-10 15:26:11 +0800

    Add files via upload

--- app/src/main/java/nQuant/android/MainActivity.java
+++ app/src/main/java/nQuant/android/MainActivity.java
@@ -1,85 +1,105 @@
 package nQuant.android;
 
-import android.app.ProgressDialog;
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
-import android.os.AsyncTask;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.android.nQuant.PnnLABQuantizer;
 import com.android.nQuant.PnnQuantizer;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     Button button;
     ImageView image;
+    String filePath;
+
+    final static int IMPORT_PICTURE = 10001;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == IMPORT_PICTURE && resultCode == RESULT_OK && null != data) {
+            filePath = data.getDataString();
+            image.setImageURI(Uri.parse(filePath));
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        File file = new File(getCacheDir(), "sample.jpg");
+        if (file.exists())
+            file.delete();
+
+        filePath = file.getAbsolutePath();
+        InputStream asset = getResources().openRawResource(+R.drawable.sample);
+        FileOutputStream output = null;
+        try {
+            output = new FileOutputStream(file);
+            final byte[] buffer = new byte[1024];
+            int size;
+            while ((size = asset.read(buffer)) != -1) {
+                output.write(buffer, 0, size);
+            }
+            asset.close();
+            output.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        image = findViewById(R.id.imageView1);
+        image.setClickable(true);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                if("Quit".equals(button.getText()))
+                    return;
+
+                try {
+                    Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent.setType("image/*");
+                    startActivityForResult(Intent.createChooser(intent, "Please select Image"), IMPORT_PICTURE);
+                } catch (android.content.ActivityNotFoundException ex) {
+                    // Potentially direct the user to the Market with a Dialog
+                    Toast.makeText(getApplicationContext(), "Please install a File Manager.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         addListenerOnButton();
     }
 
-    private class ConversionTask extends AsyncTask<Object,Void,Bitmap> {
-        private final String filePath;
-        private final ProgressDialog dialog;
+    public ProgressBar createProgressBar() {
+        LinearLayout layout = findViewById(R.id.display);
+        ProgressBar progressBar = progressBar = new ProgressBar(this, null, android.R.attr.progressBarStyleLarge);
+        layout.addView(progressBar);
 
-        public ConversionTask(MainActivity activity, String filePath) {
-            this.filePath = filePath;
-            dialog = new ProgressDialog(activity);
-        }
-
-        @Override
-        protected void onPreExecute() {
-            setProgressBarIndeterminateVisibility(true);
-            dialog.setMessage("Converting...");
-            dialog.show();
-        }
-
-        @Override
-        protected void onPostExecute(Bitmap result) {
-            setProgressBarIndeterminateVisibility(false);
-            image.setImageBitmap(result);
-            image.getLayoutParams().width = getResources().getDisplayMetrics().widthPixels;
-            image.getLayoutParams().height = (image.getLayoutParams().width * result.getHeight()) / result.getWidth();
-            image.setScaleType(ImageView.ScaleType.FIT_XY);
-            button.setText("Quit");
-            button.setEnabled(true);
-
-            if (dialog.isShowing())
-                dialog.dismiss();
-        }
-
-        @Override
-        protected Bitmap doInBackground(Object... params) {
-            try {
-                PnnQuantizer pnnQuantizer = new PnnLABQuantizer(filePath);
-                return pnnQuantizer.convert(256, true);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            } catch (Exception e) {
-                throw e;
-            }
-        }
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE, WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+        progressBar.setVisibility(View.VISIBLE);
+        return progressBar;
     }
 
     public void addListenerOnButton() {
-        image = (ImageView) findViewById(R.id.imageView1);
-
-        button = (Button) findViewById(R.id.btnChangeImage);
+        button = findViewById(R.id.btnChangeImage);
         button.setTransformationMethod(null);
         button.setOnClickListener(new OnClickListener() {
 
@@ -94,24 +114,41 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     button.setEnabled(false);
 
-                    File file = new File(getCacheDir(), "sample.jpg");
-                    if (!file.exists()) {
-                        InputStream asset = getResources().openRawResource(+ R.drawable.sample);
-                        FileOutputStream output = new FileOutputStream(file);
-                        final byte[] buffer = new byte[1024];
-                        int size;
-                        while ((size = asset.read(buffer)) != -1) {
-                            output.write(buffer, 0, size);
+                    final ProgressBar progressBar = createProgressBar();
+                    final Handler handler = new Handler(getMainLooper());
+
+                    final ExecutorService executor = Executors.newSingleThreadExecutor();
+                    executor.execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                PnnQuantizer pnnQuantizer = new PnnQuantizer(filePath);
+                                final Bitmap result = pnnQuantizer.convert(256, true);
+
+                                handler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        image.setImageBitmap(result);
+                                        image.getLayoutParams().width = getResources().getDisplayMetrics().widthPixels;
+                                        image.getLayoutParams().height = (image.getLayoutParams().width * result.getHeight()) / result.getWidth();
+                                        image.setScaleType(ImageView.ScaleType.FIT_XY);
+                                        button.setText("Quit");
+                                        button.setEnabled(true);
+
+                                        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                                        progressBar.setVisibility(View.GONE);
+                                    }
+                                });
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            } catch (Exception e) {
+                                throw e;
+                            }
                         }
-                        asset.close();
-                        output.close();
-                    }
-
-                    new ConversionTask(MainActivity.this, file.getAbsolutePath()).execute();
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    Toast.makeText(MainActivity.this, "Error! " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    });
+                } catch (Throwable t) {
+                    t.printStackTrace();
+                    Toast.makeText(MainActivity.this, "Error! " + t.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
