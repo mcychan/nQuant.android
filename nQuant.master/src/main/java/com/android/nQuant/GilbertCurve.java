faf1fb835259b5e624e38e33f57f1366dabebe59
Miller Cy Chan <miller.chan@gmail.com>
2021-12-02 12:33:01 +0800

    Update GilbertCurve.java

--- nQuant.master/src/main/java/com/android/nQuant/GilbertCurve.java
+++ nQuant.master/src/main/java/com/android/nQuant/GilbertCurve.java
@@ -35,6 +35,7 @@ public class GilbertCurve {
 	private final Ditherable ditherable;
 	private final Queue<ErrorBox> errorq;
 	private final float[] weights;
+	private final int[] lookup;
 
 	private static final byte DITHER_MAX = 9;
 	private static final float BLOCK_SIZE = 343f;	    
@@ -51,6 +52,7 @@ public class GilbertCurve {
 		this.ditherable = ditherable;	        
 		errorq = new ArrayDeque<>();
 		weights = new float[DITHER_MAX];
+		lookup = new int[65536];
 	}
 
 	private static int sign(int x) {
@@ -76,7 +78,14 @@ public class GilbertCurve {
 		int a_pix = (int) Math.min(Byte.MAX_VALUE, Math.max(error.p[3], 0.0));
 		
 		int c2 = Color.argb(a_pix, r_pix, g_pix, b_pix);
-		qPixels[bidx] = ditherable.nearestColorIndex(palette, c2);
+		if (palette.length < 64) {
+			int offset = ditherable.getColorIndex(c2);
+			if (lookup[offset] == 0)
+				lookup[offset] = (Color.alpha(c2) == 0) ? 1 : ditherable.nearestColorIndex(palette, c2) + 1;
+			qPixels[bidx] = (short) (lookup[offset] - 1);
+		}
+		else        	
+			qPixels[bidx] = ditherable.nearestColorIndex(palette, c2);
 
 		errorq.poll();
 		c2 = palette[qPixels[bidx]];
