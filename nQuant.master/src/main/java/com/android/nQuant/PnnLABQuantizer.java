e14de00a8374cc12adef79d39b5f2b3e3542251c
Miller Cy Chan <miller.chan@gmail.com>
2022-07-27 14:04:37 +0800

    Update PnnLABQuantizer.java

--- nQuant.master/src/main/java/com/android/nQuant/PnnLABQuantizer.java
+++ nQuant.master/src/main/java/com/android/nQuant/PnnLABQuantizer.java
@@ -474,7 +474,9 @@ public class PnnLABQuantizer extends PnnQuantizer {
 	{
 		int[] qPixels;		
 		Ditherable ditherable = getDitherFn();
-		if(palette.length <= 32 || (semiTransCount * 1.0 / cPixels.length) > .099)
+		if((semiTransCount * 1.0 / cPixels.length) > .099)
+			qPixels = GilbertCurve.dither(width, height, cPixels, palette, ditherable, 1.5f);
+		else if(palette.length <= 32)
 			qPixels = GilbertCurve.dither(width, height, cPixels, palette, ditherable, 1.25f);
 		else
 			qPixels = GilbertCurve.dither(width, height, cPixels, palette, ditherable);
