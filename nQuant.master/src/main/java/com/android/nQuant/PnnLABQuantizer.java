fe8e1591a4fb84d70482a90fe9ea12f9428e83f1
Miller Cy Chan <miller.chan@gmail.com>
2022-07-28 09:46:19 +0800

    Update PnnLABQuantizer.java

--- nQuant.master/src/main/java/com/android/nQuant/PnnLABQuantizer.java
+++ nQuant.master/src/main/java/com/android/nQuant/PnnLABQuantizer.java
@@ -414,13 +414,36 @@ public class PnnLABQuantizer extends PnnQuantizer {
 
 				double err = 0.0;
 				if(hasSemiTransparency || pos % 2 == 0) {
-					if(hasSemiTransparency)
+					if(hasSemiTransparency) {
 						err += PA * BitmapUtilities.sqr(Color.alpha(c2) - Color.alpha(c));
-					err += PR * BitmapUtilities.sqr(Color.red(c2) - Color.red(c)) + PG * BitmapUtilities.sqr(Color.green(c2) - Color.green(c)) + PB * BitmapUtilities.sqr(Color.blue(c2) - Color.blue(c));
+						if (err >= closest[3])
+							continue;
+					}
+					err += PR * BitmapUtilities.sqr(Color.red(c2) - Color.red(c));
+					
+					if (err >= closest[3])
+						continue;
+					err += PG * BitmapUtilities.sqr(Color.green(c2) - Color.green(c));
+					
+					if (err >= closest[3])
+						continue;
+					err += PB * BitmapUtilities.sqr(Color.blue(c2) - Color.blue(c));
 				}
 				else {
-					for (short i = 0; i < coeffs.length; ++i)
-						err += BitmapUtilities.sqr(coeffs[i][0] * (Color.red(c2) - Color.red(c))) + BitmapUtilities.sqr(coeffs[i][1] * (Color.green(c2) - Color.green(c))) + BitmapUtilities.sqr(coeffs[i][2] * (Color.blue(c2) - Color.blue(c)));
+					for (short i = 0; i < coeffs.length; ++i) {
+						err += BitmapUtilities.sqr(coeffs[i][0] * (Color.red(c2) - Color.red(c)));
+						if (err >= closest[3])
+							break;
+						err += BitmapUtilities.sqr(coeffs[i][1] * (Color.green(c2) - Color.green(c)));
+						if (err >= closest[3])
+							break;
+						err += BitmapUtilities.sqr(coeffs[i][2] * (Color.blue(c2) - Color.blue(c)));
+						if (err >= closest[3])
+							break;
+					}
+					
+					if (err >= closest[3])
+						continue;
 				}
 
 				if (err < closest[2]) {
