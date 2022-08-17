528c1897fd6ea237aec7cb15c8d17530ea560866
Miller Cy Chan <miller.chan@gmail.com>
2022-08-17 09:09:54 +0800

    Update PnnLABQuantizer.java

--- nQuant.master/src/main/java/com/android/nQuant/PnnLABQuantizer.java
+++ nQuant.master/src/main/java/com/android/nQuant/PnnLABQuantizer.java
@@ -412,7 +412,7 @@ public class PnnLABQuantizer extends PnnQuantizer {
 			closest[2] = closest[3] = Integer.MAX_VALUE;
 			
 			int start = 0;
-			if(BlueNoise.RAW_BLUE_NOISE[pos & 63] > 6)
+			if(BlueNoise.RAW_BLUE_NOISE[pos & 4095] > 0)
 				start = 1;
 
 			for (; k < palette.length; ++k) {
