35e9e54d84b6a9a61e4aa3e14258a4a37fa421c5
Miller Cy Chan <miller.chan@gmail.com>
2022-08-28 09:36:54 +0800

    Update PnnQuantizer.java

--- nQuant.master/src/main/java/com/android/nQuant/PnnQuantizer.java
+++ nQuant.master/src/main/java/com/android/nQuant/PnnQuantizer.java
@@ -70,7 +70,7 @@ public class PnnQuantizer {
 		double wb = bin1.bc;
 		
 		int start = 0;
-		if(BlueNoise.RAW_BLUE_NOISE[idx & 4095] > -72)
+		if(BlueNoise.RAW_BLUE_NOISE[idx & 4095] > -77)
 			start = 1;
 		
 		for (int i = bin1.fw; i != 0; i = bins[i].fw) {
