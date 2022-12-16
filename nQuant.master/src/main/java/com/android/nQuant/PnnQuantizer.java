aab386b617912074693bca268fb901cd7a9f4e04
Miller Cy Chan <miller.chan@gmail.com>
2022-12-16 09:28:26 +0800

    Update PnnQuantizer.java

--- nQuant.master/src/main/java/com/android/nQuant/PnnQuantizer.java
+++ nQuant.master/src/main/java/com/android/nQuant/PnnQuantizer.java
@@ -376,7 +376,7 @@ public class PnnQuantizer {
 		else if (closest[0] > closest[1])
 			idx = pos % 2;
 
-		if(closest[idx + 2] >= MAX_ERR || (closest[idx] == 0 && Color.alpha(c) > 0))
+		if(closest[idx + 2] >= MAX_ERR)
 			return nearestColorIndex(palette, c, pos);
 		return (short) closest[idx];
 	}
