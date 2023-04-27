9cd0602332bcff588f57dca946f30aff05254945
Miller Cy Chan <miller.chan@gmail.com>
2023-04-27 14:06:21 +0800

    Add files via upload

--- nQuant.master/src/main/java/com/android/nQuant/GilbertCurve.java
+++ nQuant.master/src/main/java/com/android/nQuant/GilbertCurve.java
@@ -80,7 +80,6 @@ public class GilbertCurve {
 		int g_pix = (int) Math.min(BYTE_MAX, Math.max(error.p[1], 0.0));
 		int b_pix = (int) Math.min(BYTE_MAX, Math.max(error.p[2], 0.0));
 		int a_pix = (int) Math.min(BYTE_MAX, Math.max(error.p[3], 0.0));
-		hasAlpha |= a_pix < 255;
 		
 		int c2 = Color.argb(a_pix, r_pix, g_pix, b_pix);
 		if (palette.length <= 32 && a_pix > 0xF0) {
