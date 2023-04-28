d4323936645e5951fe70908ed3f3ca99a6c836f9
Miller Cy Chan <miller.chan@gmail.com>
2023-04-28 09:53:53 +0800

    Update GilbertCurve.java

--- nQuant.master/src/main/java/com/android/nQuant/GilbertCurve.java
+++ nQuant.master/src/main/java/com/android/nQuant/GilbertCurve.java
@@ -108,8 +108,8 @@ public class GilbertCurve {
 		double yDiff = diffuse ? 1 : CIELABConvertor.Y_Diff(c1, c2);
 
 		int errLength = denoise ? error.p.length - 1 : 0;
-		double offset = Math.floor((1 - yDiff) * 3);
-		byte ditherMax = (hasAlpha || DITHER_MAX > 9) ? (byte) BitmapUtilities.sqr(Math.sqrt(DITHER_MAX) + offset) : DITHER_MAX;
+		double edge = hasAlpha ? 1 : Math.floor((1 - yDiff) * 3) - .25;
+		byte ditherMax = (hasAlpha || DITHER_MAX > 9) ? (byte) BitmapUtilities.sqr(Math.sqrt(DITHER_MAX) + edge) : DITHER_MAX;
 		for(int j = 0; j < errLength; ++j) {
 			if(Math.abs(error.p[j]) >= ditherMax) {
 				if (diffuse)
