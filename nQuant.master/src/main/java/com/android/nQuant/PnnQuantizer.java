422115213edf7d242017e41c9c1b595a4bc3867b
Miller Cy Chan <miller.chan@gmail.com>
2023-05-03 21:07:37 +0800

    Update PnnQuantizer.java

--- nQuant.master/src/main/java/com/android/nQuant/PnnQuantizer.java
+++ nQuant.master/src/main/java/com/android/nQuant/PnnQuantizer.java
@@ -176,9 +176,7 @@ public class PnnQuantizer {
 		if(nMaxColors < 16)
 			quan_rt = -1;
 		
-		weight = nMaxColors * 1.0 / maxbins;
-		if (weight > .003 && weight < .005)
-			quan_rt = 0;
+		weight = Math.min(0.9, nMaxColors * 1.0 / maxbins);
 		if (weight < .04 && PG >= coeffs[0][1]) {
 			PR = PG = PB = PA = 1;
 			if (nMaxColors >= 64)
