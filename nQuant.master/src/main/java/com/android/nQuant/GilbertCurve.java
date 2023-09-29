835f9fd58ac3665ff3bcf957de12b9df70bb7b02
Miller Cy Chan <miller.chan@gmail.com>
2023-09-29 16:42:30 +0800

    Update GilbertCurve.java

--- nQuant.master/src/main/java/com/android/nQuant/GilbertCurve.java
+++ nQuant.master/src/main/java/com/android/nQuant/GilbertCurve.java
@@ -57,7 +57,7 @@ public class GilbertCurve {
 		this.ditherable = ditherable;
 		this.saliencies = saliencies;
 		boolean hasAlpha = weight < 0;
-		sortedByYDiff = !hasAlpha && palette.length >= 64;
+		sortedByYDiff = !hasAlpha && palette.length >= 64 && weight < .09;
 		errorq = sortedByYDiff ? new PriorityQueue<>(new Comparator<ErrorBox>() {
 
 			@Override
