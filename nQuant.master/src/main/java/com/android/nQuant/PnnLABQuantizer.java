4d5d9cb640e56d002f9899f31c37cb5944c22edf
Miller Cy Chan <miller.chan@gmail.com>
2024-07-24 12:15:18 +0800

    Update PnnLABQuantizer.java

--- nQuant.master/src/main/java/com/android/nQuant/PnnLABQuantizer.java
+++ nQuant.master/src/main/java/com/android/nQuant/PnnLABQuantizer.java
@@ -230,7 +230,7 @@ public class PnnLABQuantizer extends PnnQuantizer {
 				ratio = Math.min(1.0, 1.0 - weight);
 			else if(proportional > .04)
 				ratio = Math.min(1.0, weight * Math.exp(1.56));
-			else if(proportional > .025 && weight < .002)
+			else if(proportional > .025 && (weight < .002 || weight > .0022))
 				ratio = Math.min(1.0, proportional + weight * Math.exp(3.66));
 			else
 				ratio = Math.min(1.0, proportional + weight * Math.exp(1.718));
