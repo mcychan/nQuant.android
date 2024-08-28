3bc18bd9e6bb60887c29643c45a7878637083f77
Miller Cy Chan <miller.chan@gmail.com>
2024-08-28 23:53:52 +0800

    Update GilbertCurve.java

--- nQuant.master/src/main/java/com/android/nQuant/GilbertCurve.java
+++ nQuant.master/src/main/java/com/android/nQuant/GilbertCurve.java
@@ -59,7 +59,7 @@ public class GilbertCurve {
 		boolean hasAlpha = weight < 0;
 		weight = Math.abs(weight);
 		margin = weight < .0025 ? 12 : 6;
-		sortedByYDiff = palette.length >= 128 && (hasAlpha ? weight < .18 : weight >= .04);
+		sortedByYDiff = palette.length >= 128 && (hasAlpha ? weight < .18 : weight >= .052);
 		errorq = sortedByYDiff ? new PriorityQueue<>(new Comparator<ErrorBox>() {
 
 			@Override
