2c0f5bd629121752301692453445c20490c2ec33
Miller Cy Chan <miller.chan@gmail.com>
2024-09-30 21:16:46 +0800

    Update GilbertCurve.java

--- nQuant.master/src/main/java/com/android/nQuant/GilbertCurve.java
+++ nQuant.master/src/main/java/com/android/nQuant/GilbertCurve.java
@@ -114,7 +114,7 @@ public class GilbertCurve {
 				lookup[offset] = ditherable.nearestColorIndex(palette, c2, bidx) + 1;
 			qPixels[bidx] = palette[lookup[offset] - 1];
 
-			int acceptedDiff = Math.max(1, palette.length - margin);
+			int acceptedDiff = Math.max(2, palette.length - margin);
 			if(saliencies != null && (CIELABConvertor.Y_Diff(pixel, c2) > acceptedDiff || CIELABConvertor.U_Diff(pixel, c2) > (2 * acceptedDiff))) {
 				final float strength = 1 / 3f;
 				c2 = BlueNoise.diffuse(pixel, palette[qPixels[bidx]], 1 / saliencies[bidx], strength, x, y);
