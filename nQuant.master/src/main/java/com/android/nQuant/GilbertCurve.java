81071395df105c83a4bb46936bc256f6b449cb97
Miller Cy Chan <miller.chan@gmail.com>
2025-02-14 11:38:26 +0800

    Update GilbertCurve.java

--- nQuant.master/src/main/java/com/android/nQuant/GilbertCurve.java
+++ nQuant.master/src/main/java/com/android/nQuant/GilbertCurve.java
@@ -79,9 +79,9 @@ public class GilbertCurve {
 		double deviation = weight > .002 ? -.25 : 1;
 		ditherMax = (hasAlpha || DITHER_MAX > 9) ? (byte) BitmapUtilities.sqr(Math.sqrt(DITHER_MAX) + edge * deviation) : DITHER_MAX;
 		final int density = palette.length > 16 ? 3200 : 1500;
-		if(weight < .03 && palette.length / weight > 5000 && weight > .01 && palette.length <= 64)
+		if(palette.length / weight > 5000 && (weight < .045 || (weight > .01 && palette.length <= 64)))
 			ditherMax = (byte) BitmapUtilities.sqr(5 + edge);
-		else if(palette.length / weight < density && palette.length >= 16 && palette.length < 256)
+		else if(weight < .03 && palette.length / weight < density && palette.length >= 16 && palette.length < 256)
 			ditherMax = (byte) BitmapUtilities.sqr(5 + edge);
 		thresold = DITHER_MAX > 9 ? -112 : -64;
 		weights = new float[0];
