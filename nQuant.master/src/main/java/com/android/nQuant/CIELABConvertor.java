44191c7496fa646033cbfcd3f752fee2bebb7c30
Miller Cy Chan <miller.chan@gmail.com>
2025-03-06 14:05:10 +0800

    Update CIELABConvertor.java

--- nQuant.master/src/main/java/com/android/nQuant/CIELABConvertor.java
+++ nQuant.master/src/main/java/com/android/nQuant/CIELABConvertor.java
@@ -223,7 +223,7 @@ public class CIELABConvertor {
 		
 		double y = color2Y.apply(c1);
 		double y2 = color2Y.apply(c2);
-		return (y2 - y) * XYZ_WHITE_REFERENCE_Y;
+		return Math.abs(y2 - y) * XYZ_WHITE_REFERENCE_Y;
 	}
 
 	static double U_Diff(final int c1, final int c2)
@@ -234,6 +234,6 @@ public class CIELABConvertor {
 		
 		double u = color2U.apply(c1);
 		double u2 = color2U.apply(c2);
-		return u2 - u;
+		return Math.abs(u2 - u);
 	}
 }
