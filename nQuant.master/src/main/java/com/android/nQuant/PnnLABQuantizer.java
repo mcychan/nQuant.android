e417c112bcefbbde2d16207f4af8255cac8e1cea
Miller Cy Chan <miller.chan@gmail.com>
2025-03-19 16:01:23 +0800

    Update PnnLABQuantizer.java

--- nQuant.master/src/main/java/com/android/nQuant/PnnLABQuantizer.java
+++ nQuant.master/src/main/java/com/android/nQuant/PnnLABQuantizer.java
@@ -504,7 +504,7 @@ public class PnnLABQuantizer extends PnnQuantizer {
 		if(hasSemiTransparency)
 			weight *= -1;
 
-		if(dither && !hasSemiTransparency && saliencies == null && (weight < .052 || weight > .99)) {
+		if(dither && !hasSemiTransparency && saliencies == null && (palette.length <= 128 || weight > .99)) {
 			saliencies = new float[pixels.length];
 			float saliencyBase = .1f;
 
