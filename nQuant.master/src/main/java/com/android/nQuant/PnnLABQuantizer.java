6f341792d4a5e79fcf8940a9421c040055012fdd
Miller Cy Chan <miller.chan@gmail.com>
2025-12-24 10:32:54 +0800

    Update PnnLABQuantizer.java

--- nQuant.master/src/main/java/com/android/nQuant/PnnLABQuantizer.java
+++ nQuant.master/src/main/java/com/android/nQuant/PnnLABQuantizer.java
@@ -215,7 +215,7 @@ public class PnnLABQuantizer extends PnnQuantizer {
 		}
 		bins[j].cnt = quanFn.get(bins[j].cnt);
 
-		final boolean texicab = proportional > .0225;
+		final boolean texicab = proportional > .0225 && !hasSemiTransparency;
 		
 		if(hasSemiTransparency)
 			ratio = .5;
